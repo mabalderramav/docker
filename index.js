8b2226365da692ffea92c4cd9e5ed5dfd6628825
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-11-01 21:45:14 -0400

    docker compose

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/test";
 app.get("/", (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send("💥 BOOM !Aldo other thing 💥: " + err);
+      res.status(500).send("💥 BOOM1 !Aldo other thing 💥: " + err);
     } else {
-      res.send("Me conecté a la DB! 😎");
+      res.send("Me conecté aldo aldo aldo a la DB! 😎");
       db.close();
     }
   });
